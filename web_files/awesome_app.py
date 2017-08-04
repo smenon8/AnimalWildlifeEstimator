26a9a75f6304c3daaa92f8ef70bfa2ca838cdc80
Sreejith Menon <smenon8@uic.edu>
2017-08-03 21:08:50 -0400

    upload functionality

--- web_files/awesome_app.py
+++ web_files/awesome_app.py
@@ -1,6 +1,6 @@
-from flask import Flask, render_template, request, json
+from flask import Flask, render_template, request, json, flash
 
-import sys
+import sys, os
 sys.path.append("../script")
 
 import mongod_helper as mh
@@ -8,9 +8,9 @@ import MarkRecapHelper as mark
 
 app = Flask(__name__)
 app.debug = True
-
-def get_days_dict():
-    pass
+app.config['UPLOAD_FOLDER'] = '/tmp/'
+app.config['ALLOWED_EXTENSIONS'] = set(['json'])
+app.secret_key = 'some_secret'
 
 @app.route("/")
 def main():
@@ -21,6 +21,26 @@ def main():
 def showEstimation():
     return render_template("estimation.html")
 
+@app.route("/showUploader")
+def showUploader():
+    return render_template("upload_json.html")
+
+@app.route("/upload", methods=["POST", "GET"])
+def upload():
+    exifFile = request.files['ExifFile']
+    mapFile = request.files['MapFile']
+
+    source_name = request.form['inputSourceName']
+
+
+    exifFile.save(os.path.join(app.config['UPLOAD_FOLDER'], exifFile.filename))
+    print("File %s uploaded" %exifFile.filename)
+
+    mapFile.save(os.path.join(app.config['UPLOAD_FOLDER'], mapFile.filename))
+    print("File %s uploaded" % mapFile.filename)
+
+
+    return render_template("upload_json.html", msg=exifFile.filename + " & "  + mapFile.filename)
 
 @app.route("/estimation", methods=["POST", "GET"])
 def estimation():
@@ -40,7 +60,6 @@ def estimation():
     outputLine3  = "<b>Estimated Population =</b> %f" %estimate[2]
     output = "<p>%s<br />%s<br />%s<br />%s<br />" %(outputLine0, outputLine1, outputLine2, outputLine3)
 
-    print(output)
     return output
 
 if __name__ =="__main__":
